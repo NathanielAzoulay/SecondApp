cf4e1f4f8bf3e2d06e089f4f551ec37a8b6664ce
naorb <naorbarkochva@gmail.com>
2021-01-07 12:21:15 +0200

    fixed crash in CompanyTravelsFragment

--- app/src/main/java/com/example/travel_app_secondapp/ui/companyTravels/CompanyTravelsFragment.java
+++ app/src/main/java/com/example/travel_app_secondapp/ui/companyTravels/CompanyTravelsFragment.java
@@ -72,7 +72,7 @@ public class CompanyTravelsFragment extends Fragment {
 
         userEmail = parentActivity.getUserEmail();
         companyTravelsViewModel = new ViewModelProvider(this).get(CompanyTravelsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_travels_registered, container, false);
+        View root = inflater.inflate(R.layout.fragment_travels_company, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
 
 
