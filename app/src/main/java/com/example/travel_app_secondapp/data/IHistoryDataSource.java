c61896b4454f9a3b5cd8f748b29523b27a371d8e
naorb <naorbarkochva@gmail.com>
2021-01-15 02:20:29 +0200

    service in manifest

--- app/src/main/java/com/example/travel_app_secondapp/data/IHistoryDataSource.java
+++ app/src/main/java/com/example/travel_app_secondapp/data/IHistoryDataSource.java
@@ -7,15 +7,15 @@ import com.example.travel_app_secondapp.entities.Travel;
 import java.util.List;
 
 public interface IHistoryDataSource {
-    public void addTravel(Travel p);
+    void addTravel(Travel p);
 
-    public void addTravel(List<Travel> travelList);
+    void addTravel(List<Travel> travelList);
 
-    public void editTravel(Travel p);
+    void editTravel(Travel p);
 
-    public void deleteTravel(Travel p);
+    void deleteTravel(Travel p);
 
-    public void clearTable();
+    void clearTable();
 
-    public LiveData<List<Travel>> getTravels();
+    LiveData<List<Travel>> getTravels();
 }
